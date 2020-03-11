12b999131cc250b5c230a2988ffdb8ba8983e248
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2020-03-11 16:34:24 +0100

    docs: fix typo, paramters -> parameters

--- google/cloud/storage/blob.py
+++ google/cloud/storage/blob.py
@@ -445,7 +445,7 @@ class Blob(_PropertyMixin):
 
         :type query_parameters: dict
         :param query_parameters:
-            (Optional) Additional query paramtersto be included as part of the
+            (Optional) Additional query parameters to be included as part of the
             signed URLs.  See:
             https://cloud.google.com/storage/docs/xml-api/reference-headers#query
 
