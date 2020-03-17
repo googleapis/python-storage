9827d3b4bcbc19dc3499a8fdfe58951b794869a6
Chris Wilcox <crwilcox@google.com>
2020-03-17 14:46:27 -0700

    fix: use the google-crc32c package

--- setup.py
+++ setup.py
@@ -32,9 +32,9 @@ dependencies = [
     "google-auth >= 1.11.0, < 2.0dev",
     "google-cloud-core >= 1.2.0, < 2.0dev",
     "google-resumable-media >= 0.5.0, < 0.6dev",
-    "crcmod",
+    "google-crc32c",
 ]
-extras = {"crc32c": "google-crc32c"}
+extras = {}
 
 
 # Setup boilerplate below this line.
