aa9b47134d2a6a94ae74c19b699f6717d6b181e2
yoshi-automation <yoshi-automation@google.com>
2020-06-24 14:42:13 -0700

    feat(python): add defaults for microgenerator libraries
    Source-Author: Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
    Source-Date: Mon Jun 22 17:56:27 2020 -0700
    Source-Repo: googleapis/synthtool
    Source-Sha: db4f15fedc76e04018e6e43ec403e804e1142158
    Source-Link: https://github.com/googleapis/synthtool/commit/db4f15fedc76e04018e6e43ec403e804e1142158

--- noxfile.py
+++ noxfile.py
@@ -65,8 +65,7 @@ def lint_setup_py(session):
 
 
 def default(session):
-    # Install all test dependencies, then install this package in-place.
-    session.install("mock", "pytest", "pytest-cov")
+    # Install all test dependencies, then install this package in-place.session.install("mock", "pytest", "pytest-cov")
     session.install("-e", ".")
 
     # Run py.test against the unit tests.
