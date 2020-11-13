d1a773bfa50b6e314fbea290868ba4063696d659
Tres Seaver <tseaver@palladion.com>
2020-11-13 17:34:01 -0500

    chore: restore 2.7 classifier

--- setup.py
+++ setup.py
@@ -75,6 +75,8 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
