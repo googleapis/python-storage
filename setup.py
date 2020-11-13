3b73ccf7ce4139fbc1e87a44eb1df50674387177
Tres Seaver <tseaver@palladion.com>
2020-11-13 17:32:32 -0500

    chore: restore 2.7 support

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ setuptools.setup(
     namespace_packages=namespaces,
     install_requires=dependencies,
     extras_require=extras,
-    python_requires=">=3.6",
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
     include_package_data=True,
     zip_safe=False,
 )
