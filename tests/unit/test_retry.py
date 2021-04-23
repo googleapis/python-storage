7bf2c39b32b626387b65f6afbec17d1485497224
Frank Natividad <franknatividad@google.com>
2021-04-23 14:51:33 -0700

    fix unit test

--- tests/unit/test_retry.py
+++ tests/unit/test_retry.py
@@ -25,14 +25,14 @@ class Test_should_retry(unittest.TestCase):
 
         return retry._should_retry(exc)
 
-    def test_w_retryable_types(self):
+    def test_w_retryable_transport_error(self):
         from google.cloud.storage import retry
         from google.auth.exceptions import TransportError as eTransportError
         from requests import ConnectionError as rConnectionError
 
         caught_exc = rConnectionError("Remote end closed connection unexpected")
         exc = eTransportError(caught_exc)
-        self.assertTrue(self._call_fut(exc))
+        self.assertTrue(retry._should_retry(exc))
 
     def test_w_wrapped_type(self):
         from google.cloud.storage import retry
