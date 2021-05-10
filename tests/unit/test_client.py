afdbd672d761310a7ed2d8b72eb2c7e353bf5930
Tres Seaver <tseaver@palladion.com>
2021-05-10 16:12:59 -0400

    refactor: add 'Client._post_resource' method
    Toward #38.

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -593,6 +593,69 @@ class TestClient(unittest.TestCase):
             _target_object=target,
         )
 
+    def test__post_resource_miss_w_defaults(self):
+        from google.cloud.exceptions import NotFound
+
+        project = "PROJECT"
+        path = "/path/to/something"
+        credentials = _make_credentials()
+        data = {"baz": "Baz"}
+
+        client = self._make_one(project=project, credentials=credentials)
+        connection = client._base_connection = _make_connection()
+
+        with self.assertRaises(NotFound):
+            client._post_resource(path, data)
+
+        connection.api_request.assert_called_once_with(
+            method="POST",
+            path=path,
+            data=data,
+            query_params=None,
+            headers=None,
+            timeout=self._get_default_timeout(),
+            retry=DEFAULT_RETRY,
+            _target_object=None,
+        )
+
+    def test__post_resource_hit_w_explicit(self):
+        project = "PROJECT"
+        path = "/path/to/something"
+        data = {"baz": "Baz"}
+        query_params = {"foo": "Foo"}
+        headers = {"bar": "Bar"}
+        timeout = 100
+        retry = mock.Mock(spec=[])
+        credentials = _make_credentials()
+
+        client = self._make_one(project=project, credentials=credentials)
+        expected = mock.Mock(spec={})
+        connection = client._base_connection = _make_connection(expected)
+        target = mock.Mock(spec={})
+
+        found = client._post_resource(
+            path,
+            data,
+            query_params=query_params,
+            headers=headers,
+            timeout=timeout,
+            retry=retry,
+            _target_object=target,
+        )
+
+        self.assertIs(found, expected)
+
+        connection.api_request.assert_called_once_with(
+            method="POST",
+            path=path,
+            data=data,
+            query_params=query_params,
+            headers=headers,
+            timeout=timeout,
+            retry=retry,
+            _target_object=target,
+        )
+
     def test__delete_resource_miss_w_defaults(self):
         from google.cloud.exceptions import NotFound
 
