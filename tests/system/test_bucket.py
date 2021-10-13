2de90a585a5e0b06afc27ce4a2af930f3560e9f7
Aaron Gabriel Neyer <breathtender@gmail.com>
2021-10-13 15:03:40 -0600

    hmm weird

--- tests/system/test_bucket.py
+++ tests/system/test_bucket.py
@@ -890,7 +890,9 @@ def test_new_bucket_created_w_enforced_pap(
     assert not bucket.iam_configuration.uniform_bucket_level_access_enabled
 
 def test_new_bucket_with_rpo(
-    storage_client, buckets_to_delete, blobs_to_delete,
+    storage_client,
+    buckets_to_delete,
+    blobs_to_delete,
 ):
     from google.cloud.storage import constants
 
