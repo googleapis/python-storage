7f2b8fd1b7ef216db48645b0be9aea99d7353081
Aaron Gabriel Neyer <breathtender@gmail.com>
2022-01-11 14:34:27 -0700

    make python3.6 compatible

--- samples/snippets/storage_async_upload.py
+++ samples/snippets/storage_async_upload.py
@@ -55,6 +55,7 @@ async def async_upload_blob(bucket_name):
 
 
 if __name__ == "__main__":
-    asyncio.run(async_upload_blob(
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(async_upload_blob(
         bucket_name=sys.argv[1]
     ))
