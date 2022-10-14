9d442c9f7f03a255fcdd5121629401969e7b390f
rsaksham <rsaksham@google.com>
2022-10-14 12:30:10 +0530

    Update test_blob.py

--- tests/system/test_blob.py
+++ tests/system/test_blob.py
@@ -97,10 +97,7 @@ def test_large_file_write_from_stream_w_failed_checksum(
     assert not blob.exists()
 
 
-@pytest.mark.skipif(
-    "API_ENDPOINT_OVERRIDE" in os.environ,
-    reason="Failing test for the overriding endpoint",
-)
+@pytest.mark.skipif(_helpers.is_api_endpoint_override,reason="Test does not yet support endpoint override")
 def test_large_file_write_from_stream_w_encryption_key(
     storage_client,
     shared_bucket,
