8421e5ff228d0755a9d9cc922273cc068462c99d
Andrew Gorcester <gorcester@google.com>
2023-09-29 17:02:47 -0700

    docs: Add snippets for upload_chunks_concurrently and add chunk_size

--- samples/snippets/storage_transfer_manager_upload_chunks_concurrently.py
+++ samples/snippets/storage_transfer_manager_upload_chunks_concurrently.py
@@ -12,20 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [START storage_transfer_manager_download_chunks_concurrently]
-def download_chunks_concurrently(
-    bucket_name, blob_name, filename, chunk_size=32 * 1024 * 1024, processes=8
+# [START storage_transfer_manager_upload_chunks_concurrently]
+def upload_chunks_concurrently(
+    bucket_name,
+    source_filename,
+    destination_blob_name,
+    chunk_size=32 * 1024 * 1024,
+    processes=8,
 ):
-    """Download a single file in chunks, concurrently in a process pool."""
-
+    """Upload a single file, in chunks, concurrently in a process pool."""
     # The ID of your GCS bucket
     # bucket_name = "your-bucket-name"
 
-    # The file to be downloaded
-    # blob_name = "target-file"
+    # The path to your file to upload
+    # source_filename = "local/path/to/file"
 
-    # The destination filename or path
-    # filename = ""
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
 
     # The size of each chunk. The performance impact of this value depends on
     # the use case. The remote service has a minimum of 5 MiB and a maximum of
@@ -33,22 +36,21 @@ def download_chunks_concurrently(
     # chunk_size = 32 * 1024 * 1024 (32 MiB)
 
     # The maximum number of processes to use for the operation. The performance
-    # impact of this value depends on the use case, but smaller files usually
-    # benefit from a higher number of processes. Each additional process occupies
-    # some CPU and memory resources until finished.
+    # impact of this value depends on the use case. Each additional process
+    # occupies some CPU and memory resources until finished.
     # processes=8
 
     from google.cloud.storage import Client, transfer_manager
 
     storage_client = Client()
     bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(blob_name)
+    blob = bucket.blob(destination_blob_name)
 
-    transfer_manager.download_chunks_concurrently(
-        blob, filename, chunk_size=chunk_size, max_workers=processes
+    transfer_manager.upload_chunks_concurrently(
+        source_filename, blob, chunk_size=chunk_size, max_workers=processes
     )
 
-    print("Downloaded {} to {}.".format(blob_name, filename))
+    print(f"File {source_filename} uploaded to {destination_blob_name}.")
 
 
-# [END storage_transfer_manager_download_chunks_concurrently]
+# [END storage_transfer_manager_upload_chunks_concurrently]
