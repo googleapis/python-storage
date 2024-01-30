41f9f934414f02d0f7626e63ea54a5db84ee5a36
Cathy Ouyang <cathyo@google.com>
2024-01-30 15:45:44 -0800

    fix: replace utcnow in Bucket

--- google/cloud/storage/bucket.py
+++ google/cloud/storage/bucket.py
@@ -23,7 +23,6 @@ import warnings
 
 from google.api_core import datetime_helpers
 from google.cloud._helpers import _datetime_to_rfc3339
-from google.cloud._helpers import _NOW
 from google.cloud._helpers import _rfc3339_nanos_to_datetime
 from google.cloud.exceptions import NotFound
 from google.api_core.iam import Policy
@@ -83,6 +82,7 @@ _LOCATION_SETTER_MESSAGE = (
     "to `Bucket.create`."
 )
 _API_ACCESS_ENDPOINT = "https://storage.googleapis.com"
+_NOW = datetime.datetime.now
 
 
 def _blobs_page_start(iterator, page, response):
@@ -3186,7 +3186,7 @@ class Bucket(_PropertyMixin):
         _signing.ensure_signed_credentials(credentials)
 
         if expiration is None:
-            expiration = _NOW() + datetime.timedelta(hours=1)
+            expiration = _NOW(datetime.timezone.utc) + datetime.timedelta(hours=1)
 
         conditions = conditions + [{"bucket": self.name}]
 
