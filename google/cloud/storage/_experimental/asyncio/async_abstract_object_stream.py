0810afc315e29a4f9caa25c08faa7840281de545
Chandra Sirimala <chandrasiri@google.com>
2025-09-18 11:00:17 +0000

    fix doc strings, add licence and type hints

--- google/cloud/storage/_experimental/asyncio/async_abstract_object_stream.py
+++ google/cloud/storage/_experimental/asyncio/async_abstract_object_stream.py
@@ -1,36 +1,61 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import abc
+from typing import Any, Optional
 
 
 class _AsyncAbstractObjectStream(abc.ABC):
+    """Abstract base class for asynchronous object streams.
+
+    This class defines the common interface for both reading from and writing
+    to a GCS object in a streaming fashion.
+
+    :type bucket_name: str
+    :param bucket_name: (Optional) The name of the bucket containing the object.
+
+    :type object_name: str
+    :param object_name: (Optional) The name of the object.
+
+    :type generation_number: int
+    :param generation_number: (Optional) If present, selects a specific revision of
+                              this object.
     """
-    Abstract class for both ReadObjectStream as well as WriteObjectStream.
 
-    Attributes will include
-    1. bucket_name
-    2. object_name
-    3. generation_number (if given)
-
-
-    """
-
-    def __init__(self, bucket_name, object_name, generation_number=None):
+    def __init__(
+        self,
+        bucket_name: Optional[str] = None,
+        object_name: Optional[str] = None,
+        generation_number: Optional[int] = None,
+    ) -> None:
         super().__init__()
-        self.bucket_name = bucket_name
-        self.object_name = object_name
-        self.generation_number = generation_number
+        self.bucket_name: Optional[str] = bucket_name
+        self.object_name: Optional[str] = object_name
+        self.generation_number: Optional[int] = generation_number
 
     @abc.abstractmethod
-    async def open(self):
+    async def open(self) -> None:
         raise NotImplementedError("Subclasses should implement this method.")
 
     @abc.abstractmethod
-    async def close(self):
+    async def close(self) -> None:
         raise NotImplementedError("Subclasses should implement this method.")
 
     @abc.abstractmethod
-    async def send(self):
+    async def send(self, message: Any) -> None:
         raise NotImplementedError("Subclasses should implement this method.")
 
     @abc.abstractmethod
-    async def recv(self):
+    async def recv(self) -> Any:
         raise NotImplementedError("Subclasses should implement this method.")
