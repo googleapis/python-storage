3e1ce720e4f5b0d9a4c039a39f64d2f78554a963
Pulkit Aggarwal <pulkitaggarwal@google.com>
2025-11-25 09:25:33 +0000

    delete unused conftest.py

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-# Ensure there's an event loop in the main thread so grpc.aio.create_channel
-# and other code that expects a loop at import/initialization time don't fail.
-try:
-    asyncio.get_running_loop()
-except RuntimeError:
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
