60d0d7babae29fa9825a3beedfd3779d8a52c6cf
Chandra Sirimala <chandrasiri@google.com>
2025-12-18 16:02:17 +0000

    add unit test for checksum of chunk data

--- tests/unit/asyncio/test_async_appendable_object_writer.py
+++ tests/unit/asyncio/test_async_appendable_object_writer.py
@@ -15,6 +15,8 @@
 import pytest
 from unittest import mock
 
+from google_crc32c import Checksum
+
 from google.api_core import exceptions
 from google.cloud.storage._experimental.asyncio.async_appendable_object_writer import (
     AsyncAppendableObjectWriter,
@@ -452,10 +454,15 @@ async def test_append_sends_data_in_chunks(mock_write_object_stream, mock_client
     # First chunk
     assert first_call[0][0].write_offset == 100
     assert len(first_call[0][0].checksummed_data.content) == _MAX_CHUNK_SIZE_BYTES
-
+    assert first_call[0][0].checksummed_data.crc32c == int.from_bytes(
+        Checksum(data[:_MAX_CHUNK_SIZE_BYTES]).digest()
+    )
     # Second chunk
     assert second_call[0][0].write_offset == 100 + _MAX_CHUNK_SIZE_BYTES
     assert len(second_call[0][0].checksummed_data.content) == 1
+    assert second_call[0][0].checksummed_data.crc32c == int.from_bytes(
+        Checksum(data[_MAX_CHUNK_SIZE_BYTES:]).digest()
+    )
 
     assert writer.offset == 100 + len(data)
     writer.simple_flush.assert_not_awaited()
