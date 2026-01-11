e3797e4817a2131d6b9aca577f4e32a3dd2923e5
Chandra <chandrasiri@google.com>
2026-01-11 09:54:30 +0000

    add 'read_rand_multi_coros' in `test_downloads_single_proc_multi_coro`

--- tests/perf/microbenchmarks/test_reads.py
+++ tests/perf/microbenchmarks/test_reads.py
@@ -226,7 +226,8 @@ def download_files_using_json_multi_threaded(
 
 @pytest.mark.parametrize(
     "workload_params",
-    all_params["read_seq_multi_coros"],
+    all_params["read_seq_multi_coros"] + 
+    all_params["read_rand_multi_coros"],
     indirect=True,
     ids=lambda p: p.name,
 )
